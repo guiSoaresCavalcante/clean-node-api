3f991455d13b5fd10faefea524a9204f528ba371
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-04-19 16:48:02 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.json'
+      },
+      rules: {
+        '@typescript-eslint/strict-boolean-expressions': 'off'
       }
     }
   ]
