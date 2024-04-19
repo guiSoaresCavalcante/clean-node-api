7de095681e76630726d919a4ea0a63c0f19b0a3f
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-04-19 16:48:30 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,3 +15,19 @@ describe('SignUp Controller', () => {
     expect(HttpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const HttpResponse = sut.handle(httpRequest)
+    expect(HttpResponse.statusCode).toBe(400)
+    expect(HttpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
