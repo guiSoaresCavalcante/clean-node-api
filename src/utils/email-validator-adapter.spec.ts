f3d6d65ad855e2b38283060579ebb0ce3fdbe0d0
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-04-27 17:52:30 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,25 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+// mockando a lib para os testes. Aqui não importa como é feita a implementação do metodo
+// devemos sempre testar a nossa classe isolada e mockar todas as dependências
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidatorAdapter', () => {
   test('Should return false is validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@email.com')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true is validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
   })
 })
