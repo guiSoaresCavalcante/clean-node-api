580fbb719be05a93847b5f82b28c1987a478355f
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-04-27 19:41:21 -0300

    test: ensure DbAddAccount throws if encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -36,4 +36,18 @@ describe('DbAddAccount UseCase', () => {
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenLastCalledWith('valid_password')
   })
+
+  test('Should throw if Encrypt throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+
+    // mockando a dependencia do sut para retornar a exceção
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()); }))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData) // nao podemos usar o awai aqui, pq queremos pegar a exceção
+    await expect(promise).rejects.toThrow() // espero que o sut também retorne uma exeção
+  })
 })
