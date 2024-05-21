a688994649724d89323df9f8a67bf2215688b930
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-05-21 11:31:41 -0300

    test: ensure DbAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -84,4 +84,16 @@ describe('DbAddAccount UseCase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData) // nao podemos usar o awai aqui, pq queremos pegar a exceção
+    await expect(promise).rejects.toThrow() // espero que o sut também retorne uma exeção
+  })
 })
