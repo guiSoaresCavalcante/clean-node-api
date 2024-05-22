f633fca98524fe3d05e6e23d1ffe097a6c29e063
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-05-22 01:12:59 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,29 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+
+    // pegando o id inserido
+    const accountId = result.insertedId
+
+    // pegando o account
+    const account = await accountCollection.findOne({ _id: accountId })
+    if (!account) {
+      throw new Error()
+    }
+
+    const accountModel: AccountModel = {
+      id: account._id.toString(),
+      name: account.name,
+      email: account.email,
+      password: account.password
+    }
+
+    return accountModel
+  }
+}
