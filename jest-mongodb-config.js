2f2772b00c2afe589f6f5bbf4dee10b112c02163
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2024-05-22 00:21:45 -0300

    chore: change mongodb binary version

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '4.0.3', // verificar a versão do mongo e deixar igual ao de produção
+      version: '6.0.9', // verificar a versão do mongo e deixar igual ao de produção
       skipMD5: true,
     },
     instance: {
