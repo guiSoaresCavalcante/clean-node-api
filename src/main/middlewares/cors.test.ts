414659cafe44b5947f09e0fbb3a74d74327359fc
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2025-10-16 00:41:08 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,11 +1,8 @@
 import request from 'supertest'
 import app from '../config/app'
 
-// Nesse arquivo ficarao os testes de integracao. Por isso a extensao .test ao inves de .spec
-
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
-    // criando uma nova rota no nosso app (temporaria) e simulando uma requisicao
     app.post('/test_cors', (req, res) => {
       res.send()
     })
