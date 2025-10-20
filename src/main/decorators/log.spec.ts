9219778130953637e108c18048e558932c9884e0
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2025-10-20 18:45:05 -0300

    refactor: move sut creator to a factory method

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,48 @@
+import { LogControllerDecorator } from './log'
+import { Controller, HttpResponse, HttpRequest } from '../../presentation/protocols'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Guilherme'
+        }
+      }
+      return await new Promise(resolve => { resolve(httpResponse) })
+    }
+  }
+  return new ControllerStub()
+}
+
+describe('Log Controller Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const hadnleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = {
+      body: {
+        email: 'any@email',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(hadnleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
