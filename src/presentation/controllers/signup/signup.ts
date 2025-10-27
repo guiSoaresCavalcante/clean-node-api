109bdd330fc3b8aef23ff3287879c82e5fa43ee8
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2025-10-27 11:34:27 -0300

    feat: ensure SignUp Controller calls Validation with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, Controller, EmailValidator, AddAccount, HttpResponse } from './signup-protocols'
+import { HttpRequest, Controller, EmailValidator, AddAccount, HttpResponse, Validation } from './signup-protocols'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../../errors'
 
@@ -6,15 +6,18 @@ import { InvalidParamError, MissingParamError } from '../../errors'
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
+  private readonly validation: Validation
 
-  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
     // delegamos a resposabilidade de validar emails para outra classe
     this.emailValidator = emailValidator // injetando o emailValidator
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
