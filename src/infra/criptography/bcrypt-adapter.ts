8fe14dc95382e41639ec68faaf907499246c31d4
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2025-11-07 02:44:20 -0300

    feat: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,17 +1,21 @@
 import bcrypt from 'bcrypt'
 import { Hasher } from '../../data/protocols/criptography/hasher'
+import { HashComparer } from '../../data/protocols/criptography/hash-comparer'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
 
   constructor (salt: number) {
     this.salt = salt
   }
 
-  // o metodo faz parte do protocolo. Como o salt é uma propriedade especifica
-  // do Bcrypt, o colocamos no construtor
   async hash (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
     return hash
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    await bcrypt.compare(value, hash)
+    return true
   }
 }
